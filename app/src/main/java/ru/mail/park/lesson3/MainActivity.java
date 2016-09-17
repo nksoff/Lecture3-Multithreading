cdea7bbdce5b1f32a25c70d25e45c56db47e7c0b
Nikita <nks.off@gmail.com>
2016-09-17 20:40:06 +0300

    refactor a bit MainActivity

--- app/src/main/java/ru/mail/park/lesson3/MainActivity.java
+++ app/src/main/java/ru/mail/park/lesson3/MainActivity.java
@@ -3,12 +3,18 @@ package ru.mail.park.lesson3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TEXT_STATE_INIT = "ru.mail.park.lesson3.MainActivity.TEXT_STATE_INIT";
+    private final static String TEXT_STATE_LOADING = "ru.mail.park.lesson3.MainActivity.TEXT_STATE_LOADING";
+    private final static String TEXT_STATE_UNAVAILABLE = "ru.mail.park.lesson3.MainActivity.TEXT_STATE_UNAVAILABLE";
+    private final static String TEXT_STATE_LOADED = "ru.mail.park.lesson3.MainActivity.TEXT_STATE_LOADED";
 
     private static final String URL_1 = "https://gist.githubusercontent.com/anonymous/66e735b3894c5e534f2cf381c8e3165e/raw/8c16d9ec5de0632b2b5dc3e5c114d92f3128561a/gistfile1.txt";
     private static final String URL_2 = "https://gist.githubusercontent.com/anonymous/be76b41ddf012b761c15a56d92affeb6/raw/bb1d4f849cb79264b53a9760fe428bbe26851849/gistfile1.txt";
@@ -25,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView text1;
     private TextView text2;
 
+    private String text1State;
+    private String text2State;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,22 +49,8 @@ public class MainActivity extends AppCompatActivity {
         text1 = (TextView) findViewById(R.id.text1);
         text2 = (TextView) findViewById(R.id.text2);
 
-        text1.setText("Click me");
-        text2.setText("Click me");
-
-        text1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFromUrl(URL_1);
-            }
-        });
-
-        text2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadFromUrl(URL_2);
-            }
-        });
+        changeState(URL_1, TEXT_STATE_INIT);
+        changeState(URL_2, TEXT_STATE_INIT);
 
         UrlDownloader.getInstance().setCallback(new UrlDownloader.Callback() {
             @Override
@@ -65,25 +60,90 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
     private void loadFromUrl(String url) {
-        textViewForUrl(url).setText("Loading...");
+        changeState(url, TEXT_STATE_LOADING);
+    }
+
+    private void onTextLoaded(String url, String loadedText) {
+        final String state = loadedText == null ? TEXT_STATE_UNAVAILABLE : TEXT_STATE_LOADED;
+        final boolean showToast = loadedText != null;
+        changeState(url, state, loadedText);
+
+        if(showToast)
+            Toast.makeText(MainActivity.this, loadedText, Toast.LENGTH_SHORT).show();
+    }
+
+    private TextView getTextViewByUrl(String url) {
+        switch (url) {
+            case URL_1:
+                return text1;
+            case URL_2:
+                return text2;
+            default:
+                throw new IllegalArgumentException("Unknown url: " + url);
+        }
+    }
+
+    private View.OnClickListener getClickListenerForUrl(String url) {
+        final String loadUrl = url;
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFromUrl(loadUrl);
+            }
+        };
+    }
+
+    private void moveStateInit(String url) {
+        final TextView text = getTextViewByUrl(url);
+        text.setText("Click me");
+        text.setOnClickListener(getClickListenerForUrl(url));
+    }
+
+    private void moveStateLoading(String url) {
+        final TextView text = getTextViewByUrl(url);
+        text.setText("Loading...");
+        text.setOnClickListener(null);
         UrlDownloader.getInstance().load(url);
     }
 
-    private void onTextLoaded(String url, String stringFromUrl) {
-        if (stringFromUrl == null) {
-            stringFromUrl = "Data unavailable";
-        }
-        Toast.makeText(MainActivity.this, stringFromUrl, Toast.LENGTH_SHORT).show();
-        textViewForUrl(url).setText(stringFromUrl);
+    private void moveStateUnavailable(String url) {
+        final TextView text = getTextViewByUrl(url);
+        text.setText("Data unavailable");
+        text.setOnClickListener(getClickListenerForUrl(url));
     }
 
-    private TextView textViewForUrl(String url) {
-        if (URL_1.equals(url)) {
-            return text1;
-        } else if (URL_2.equals(url)) {
-            return text2;
+    private void moveStateLoaded(String url, String loadedText) {
+        final TextView text = getTextViewByUrl(url);
+        text.setText(loadedText);
+        text.setOnClickListener(null);
+    }
+
+    private void changeState(String url, String textState, @Nullable String loadedText) {
+        switch (textState) {
+            case TEXT_STATE_INIT:
+                moveStateInit(url);
+                break;
+            case TEXT_STATE_LOADING:
+                moveStateLoading(url);
+                break;
+            case TEXT_STATE_UNAVAILABLE:
+                moveStateUnavailable(url);
+                break;
+            case TEXT_STATE_LOADED:
+                moveStateLoaded(url, loadedText);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown text state: " + textState);
         }
-        throw new IllegalArgumentException("Unknown url: " + url);
+    }
+
+    private void changeState(String url, String textState) {
+        changeState(url, textState, null);
     }
 }
